067b42543f2a163434f1e9d9179a3627c9dcab26
azerid <antareza08@gmail.com>
2022-07-29 17:27:13 +0700

    Add DaggerViewModelFactory - Fungsi

--- app/src/main/java/com/needcode/githubuserapp/presenter/base/DaggerViewModelFactory.kt
+++ app/src/main/java/com/needcode/githubuserapp/presenter/base/DaggerViewModelFactory.kt
@@ -5,6 +5,11 @@ import androidx.lifecycle.ViewModelProvider
 import javax.inject.Inject
 import javax.inject.Provider
 
+
+/**
+ * berfungsi sebagai Generic-ViewmodelFactory jika di integrasikan dengan Dagger
+ */
+
 class DaggerViewModelFactory @Inject constructor(private val creators: Map<Class<out ViewModel>, @JvmSuppressWildcards Provider<ViewModel>>) :
     ViewModelProvider.Factory {
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
