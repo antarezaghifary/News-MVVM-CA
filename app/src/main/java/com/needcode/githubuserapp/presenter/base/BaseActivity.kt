d7bf2180cf8ba7ec486d7f1ce93f110c57ffd16c
azerid <antareza08@gmail.com>
2022-07-29 17:28:40 +0700

    Add Base Activity

---
+++ app/src/main/java/com/needcode/githubuserapp/presenter/base/BaseActivity.kt
@@ -0,0 +1,30 @@
+package com.needcode.githubuserapp.presenter.base
+
+import android.os.Bundle
+import androidx.databinding.ViewDataBinding
+import androidx.lifecycle.ViewModel
+import dagger.android.support.DaggerAppCompatActivity
+import javax.inject.Inject
+
+/**
+ * sebagai Class Dasar Dari Semua Activity
+ */
+
+
+abstract class BaseActivity<VM : ViewModel, BINDING : ViewDataBinding> : DaggerAppCompatActivity() {
+
+    protected lateinit var viewModel: VM
+    protected lateinit var binding: BINDING
+
+    @Inject
+    lateinit var daggerViewModelfactory: DaggerViewModelFactory
+
+    protected abstract fun createViewModel(): VM
+    protected abstract fun createBinding(): BINDING
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        viewModel = createViewModel()
+        binding = createBinding()
+    }
+
+}
