460c29c2c88edecbdc292fac4a5bf47f4084585b
azerid <antareza08@gmail.com>
2022-07-29 17:58:43 +0700

    Add ViewModelModule - Fungsi

---
+++ app/src/main/java/com/needcode/githubuserapp/di/core/module/ViewModelModule.kt
@@ -0,0 +1,24 @@
+package com.needcode.githubuserapp.di.core.module
+
+import androidx.lifecycle.ViewModel
+import com.needcode.githubuserapp.presenter.list.viewmodel.MainActivityViewModel
+import dagger.Binds
+import dagger.Module
+import dagger.multibindings.IntoMap
+
+
+/**
+ * untuk Untuk mengatur agar MainActivityViewModel bisa diakses di DaggerViewModelFactory
+ */
+@Module
+abstract class ViewModelModule {
+
+    @Module
+    abstract inner class MainActivity {
+        @Binds
+        @IntoMap
+        @ViewModelKey(MainActivityViewModel::class)
+        abstract fun bindMainActivityViewModel(vm: MainActivityViewModel): ViewModel
+    }
+
+}
