cc0a9127365b3536eae2bd6f8aad8dcc2810f2af
azerid <antareza08@gmail.com>
2022-07-29 17:53:14 +0700

    Add DatabaseModule - Fungsi

---
+++ app/src/main/java/com/needcode/githubuserapp/di/core/module/DatabaseModule.kt
@@ -0,0 +1,31 @@
+package com.needcode.githubuserapp.di.core.module
+
+import android.content.Context
+import androidx.room.Room
+import com.needcode.githubuserapp.data.source.local.AppDatabase
+import com.needcode.githubuserapp.data.source.local.dao.ListDao
+import dagger.Module
+import dagger.Provides
+import javax.inject.Singleton
+
+/**
+ * untuk menginisialisasikan AppDatabase
+ */
+
+@Module
+class DatabaseModule {
+
+    @Provides
+    @Singleton
+    fun provideAppDatabase(context: Context): AppDatabase {
+        return Room.databaseBuilder(context, AppDatabase::class.java, AppDatabase.DB_NAME)
+            .fallbackToDestructiveMigrationFrom()
+            .allowMainThreadQueries().build()
+    }
+
+    @Provides
+    fun provideListDao(database: AppDatabase): ListDao {
+        return database.listDao
+    }
+
+}
