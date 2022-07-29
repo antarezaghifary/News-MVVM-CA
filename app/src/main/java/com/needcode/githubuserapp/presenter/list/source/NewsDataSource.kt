00cc8a8b50ee9832449e85bdc207412c9ac31123
azerid <antareza08@gmail.com>
2022-07-29 17:32:05 +0700

    Add NewsDataSource - Fungsi

---
+++ app/src/main/java/com/needcode/githubuserapp/presenter/list/source/NewsDataSource.kt
@@ -0,0 +1,57 @@
+package com.needcode.githubuserapp.presenter.list.source
+
+import androidx.lifecycle.MutableLiveData
+import androidx.paging.PageKeyedDataSource
+import com.needcode.githubuserapp.data.model.NewsModel
+import com.needcode.githubuserapp.domain.usecase.ListUseCase
+import com.needcode.githubuserapp.external.LoadingState
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
+
+
+/**
+ * akan mengirim event ke useCase untuk melakukan pengambilan data dari API maupun dari LOCAL
+ * melalui function useCase.getPagedNews(). dan membuat coroutines baru sehingga tidak terjadi
+ * blocking saat useCase melakukan request ke Entity. delay dapat kalian atur sesuka hati untuk testing.
+ */
+
+
+class NewsDataSource(private val scope: CoroutineScope, private val useCase: ListUseCase) :
+    PageKeyedDataSource<Int, NewsModel>() {
+
+    var state: MutableLiveData<LoadingState> = MutableLiveData()
+
+    override fun loadInitial(
+        params: LoadInitialParams<Int>,
+        callback: LoadInitialCallback<Int, NewsModel>
+    ) {
+        scope.launch(Dispatchers.IO) {
+//            delay(1000)
+            state.postValue(LoadingState.LOADING)
+            val res = async { useCase.getPagedNews(1, params.requestedLoadSize) }
+            val list = res.await()?.toMutableList()
+            if (list != null) {
+                callback.onResult(list, null, 2)
+            }
+            state.postValue(LoadingState.DONE)
+        }
+    }
+
+    override fun loadAfter(params: LoadParams<Int>, callback: LoadCallback<Int, NewsModel>) {
+        scope.launch(Dispatchers.IO) {
+//            delay(1000)
+            state.postValue(LoadingState.LOADING)
+            val res = async { useCase.getPagedNews(params.key, params.requestedLoadSize) }
+            val list = res.await()?.toMutableList()
+            if (list != null) {
+                callback.onResult(list, params.key + 1)
+            }
+            state.postValue(LoadingState.DONE)
+        }
+    }
+
+    override fun loadBefore(params: LoadParams<Int>, callback: LoadCallback<Int, NewsModel>) {
+    }
+}
