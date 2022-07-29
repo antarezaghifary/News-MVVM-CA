3785de0508f4b2fd4101969bd865de471cb04c14
azerid <antareza08@gmail.com>
2022-07-29 17:26:34 +0700

    Add DaggerViewModelFactory, Move MainActivity to list/view

--- app/src/main/java/com/needcode/githubuserapp/presenter/list/view/MainActivity.kt
+++ app/src/main/java/com/needcode/githubuserapp/presenter/list/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.needcode.githubuserapp
+package com.needcode.githubuserapp.presenter.list.view
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.needcode.githubuserapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
