5138d878efd3a6a20beda1c8a73be8b5a29bfc99
azerid <antareza08@gmail.com>
2022-07-29 17:21:41 +0700

    Add ListUseCase - Fungsi

--- app/src/main/java/com/needcode/githubuserapp/domain/usecase/ListUseCase.kt
+++ app/src/main/java/com/needcode/githubuserapp/domain/usecase/ListUseCase.kt
@@ -5,6 +5,11 @@ import com.needcode.githubuserapp.data.model.NewsModel
 import com.needcode.githubuserapp.domain.repository.ListRepository
 import javax.inject.Inject
 
+
+/**
+ * berfungsi sebagai jembatan antara Presenter dan Entity dan melakukan perubahan data jika diperlukan
+ */
+
 class ListUseCase @Inject constructor(private val repository: ListRepository) {
     suspend fun getPagedNews(page: Int, pageSize: Int): List<NewsModel>? {
         Log.e("USECASE", "GETTING DATA FROM REPO")
