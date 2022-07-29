74f8a8908a1f5a1abc190039503be47df97e139b
azerid <antareza08@gmail.com>
2022-07-29 16:35:11 +0700

    Add Model

---
+++ app/src/main/java/com/needcode/githubuserapp/data/model/NewsListsModel.kt
@@ -0,0 +1,12 @@
+package com.needcode.githubuserapp.data.model
+
+import android.os.Parcelable
+import androidx.annotation.Keep
+import com.google.gson.annotations.SerializedName
+import kotlinx.android.parcel.Parcelize
+
+@Keep
+@Parcelize
+data class NewsListsModel(
+    @SerializedName("articles") var newsModel: List<NewsModel>
+) : Parcelable
---
+++ app/src/main/java/com/needcode/githubuserapp/data/model/NewsModel.kt
@@ -0,0 +1,13 @@
+package com.needcode.githubuserapp.data.model
+
+import android.os.Parcelable
+import androidx.annotation.Keep
+import com.google.gson.annotations.SerializedName
+import kotlinx.android.parcel.Parcelize
+
+@Keep
+@Parcelize
+data class NewsModel(
+    val title: String,
+    @SerializedName("urlToImage") val image: String?
+) : Parcelable
